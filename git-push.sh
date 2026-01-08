abb696c8526d09d83b9c40576160386771100418
amiles5 <anthony.miles@gmail.com>
2026-01-08 10:13:15 +0000

    update git push

--- git-push.sh
+++ git-push.sh
@@ -1,2 +1,2 @@
 # add changes to .gitignore if want delete something from git repo
-/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME commit ; /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME  push
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME commit -a ; /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME  push
