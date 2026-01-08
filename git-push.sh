3ce583e421347cad0ee47fc0c0c56799050df726
amiles5 <anthony.miles@gmail.com>
2026-01-08 10:08:40 +0000

    added git push script

---
+++ git-push.sh
@@ -0,0 +1,2 @@
+# add changes to .gitignore if want delete something from git repo
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME commit ; /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME  push
